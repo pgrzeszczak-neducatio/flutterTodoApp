be387a90f5f8502f3b52b2f4fe1eb753a5349af3
Przemysław Grzeszczak <pgrzeszczak@n-educatio.pl>
2018-07-29 19:42:10 +0200

    Clean up code, extract files

---
+++ lib/homepage/homepage.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import '../todolist/todolist.dart';
+
+class MyHomePage extends StatelessWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      appBar: new AppBar(
+        title: new Text(title),
+      ),
+      body: new TodoList(),
+    );
+  }
+}
+
---
+++ lib/todolist/todolist.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class TodoList extends StatefulWidget {
+  @override
+  _TodoListState createState() => new _TodoListState();
+}
+
+class _TodoListState extends State<TodoList> {
+  @override
+  Widget build(BuildContext context) {
+    return new Text('TO DZIAŁA!');
+  }
+}
